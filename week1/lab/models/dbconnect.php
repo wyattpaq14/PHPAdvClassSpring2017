4efb3e26f933835a44b098d946d15fba84e24389
Wyatt Paquin <Wyatt Paquin@192.168.200.17>
2017-03-28 15:13:35 -0400

    Lab 01 final commit

--- week1/lab/models/dbconnect.php
+++ week1/lab/models/dbconnect.php
@@ -7,9 +7,9 @@
  */
 function dbconnect() {
     $config = array(
-        'DB_DNS' => 'mysql:host=192.168.2.223;port=3306;dbname=PHPAdvClassSpring2017',
-        'DB_USER' => 'wyatt',
-        'DB_PASSWORD' => '123456'
+        'DB_DNS' => 'mysql:host=localhost;port=3306;dbname=PHPAdvClassSpring2017',
+        'DB_USER' => 'root',
+        'DB_PASSWORD' => ''
     );
 
     try {
