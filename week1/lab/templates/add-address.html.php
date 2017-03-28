9dd51cc6a850f6a9b797ac18c46b18fb1fc5518c
Wyatt Paquin <Wyatt Paquin@192.168.200.17>
2017-03-27 21:00:38 -0400

    Lab 01 progress

--- week1/lab/templates/add-address.html.php
+++ week1/lab/templates/add-address.html.php
@@ -1,23 +1,43 @@
 <!-- put your HTML here -->
 
+<div class="col-lg-6">
+    <form action="#" method="post" class="form-horizontal">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Full Name" name="fullname" aria-describedby="basic-addon1">
+        </div>
 
-<form action="#" method="post">
-    Full name: <input type="text" name="fullname" value="" /> <br />
-    Email: <input type="text" name="email" value="" /> <br />
-    Add line1: <input type="text" name="addressline1" value="" /> <br />
-    City: <input type="text" name="city" value="" /> <br />
-    state: <select name="state" >
-        <?php foreach ($states as $key => $value): ?> 
-            <option value="<?php echo $key; ?>" <?php if ($state == $key): ?> selected="selected" <?php endif; ?>><?php echo $value; ?></option>
-        <?php endforeach; ?>
-    </select><br />
-    zip: <input type="text" name="zip" value="" /> <br />
-    birthday: <input type="date" name="birthday" value="" /> <br />
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Email" name="email" aria-describedby="basic-addon1">
+        </div>
+
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Address" name="addressline1" aria-describedby="basic-addon1">
+        </div>
+
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="City" name="city" aria-describedby="basic-addon1">
+        </div>
 
 
-    <button type="submit" value="Submit" class="btn">Submit</button>
+
+        <select name="state" class="form-control" id="select" class="form-control" id="select" >
+            <?php foreach ($states as $key => $value): ?> 
+                <option value="<?php echo $key; ?>" <?php if ($state == $key): ?> selected="selected" <?php endif; ?>><?php echo $value; ?></option>
+            <?php endforeach; ?>
+        </select>
+
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Zip" name="zip" aria-describedby="basic-addon1">
+        </div>
+
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Birthday" name="birthday" aria-describedby="basic-addon1">
+        </div>
 
 
-</form>
+        <button type="submit" value="Submit" class="btn">Submit</button>
 
+
+    </form>
+</div>
 
