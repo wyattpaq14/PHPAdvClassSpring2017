c134a0ccee988cf621ee26ab50adbc4b18a615c1
Wyatt Paquin <Wyatt Paquin@192.168.200.17>
2017-04-03 19:26:52 -0400

    Week 2

---
+++ week2/demo/CRUD/addressCrud.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of addressCrud
+ *
+ * @author Wyatt Paquin
+ */
+class addressCrud implements ICrud {
+    //put your code here
+    
+    public function create() {
+        
+    }
+
+    public function delete() {
+        
+    }
+
+    public function readAll() {
+        
+    }
+
+    public function readSingle() {
+        
+    }
+
+    public function update() {
+        
+    }
+
+}
